c0046c02ac1eba3daf383e561c31155f7c839cdf
Anton <anton.mois33v@gmail.com>
2019-03-05 12:14:10 -0500

    Made code DRY by embedding Django's templates in new.html

--- reinforcement_templates/views.py
+++ reinforcement_templates/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def new_profile(request):
-    context = {}
+    form_to_fields = {"fancy_form": ["email", "username", "pin", "website", "address", "alias"]}
+    context = {"forms": form_to_fields}
     return HttpResponse(render(request, 'profiles/new.html', context))
